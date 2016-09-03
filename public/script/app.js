5409fe7ce11d261cac06b8b4e10d7e8bc9ee30b5
Luke Snider <luke946@gmail.com>
2016-09-03 12:10:43 -0700

    Re-worked entire site structure.
    Added multiple pages.

--- public/script/app.js
+++ public/script/app.js
@@ -12,19 +12,18 @@ var main = function(){
   firebase.initializeApp(config);
   firebase.auth().onAuthStateChanged(onAuthStateChanged);
 
-    $('#create-button').click(function(){
-        $('#name').toggle();
-        return false;
-    });
 
-    $('#login-button').click(function(){
+
+    $('#login_button').click(function(){
         firebase.auth().signInWithEmailAndPassword($('#email').val(), $('#password').val()).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log(errorCode+". "+errorMessage);
+            
             // ...
         });
+
         return false;
     });
 
@@ -32,10 +31,11 @@ var main = function(){
         firebase.auth().signOut().then(function() {
         // Sign-out successful.
         cleanupUi();
-        goToIndex();
+        window.location.href = 'index.html';
 
         }, function(error) {
         // An error happened.
+        console.log('error logging out');
         });
     });
     $('#complaint_link').click(function(){
@@ -46,6 +46,9 @@ var main = function(){
         $('.complaints').hide();
         $('.dashboard').fadeIn('slow');
     });
+
+
+
 }
 
 function userProfile(){
@@ -77,13 +80,22 @@ function userProfile(){
 
 function getComplaints(userg){
     var userId = firebase.auth().currentUser.uid;
-    var userComplaints = firebase.database().ref('Groups/' + userg).orderByChild('dateTime');
+    var userComplaints = firebase.database().ref('Groups/' + userg +'/').orderByChild('dateTime');
+    var complaints = [];
 
-    userComplaints.on('child_added', function _add(data) {
-        var mydata = data.val();
+
+    userComplaints.on('child_added', function(data) {
         var key = data.key;
-        formatComplaint(mydata);
 
+        if ($('#'+key).length){
+
+        }else {
+        var mydata = data.val();
+
+        //complaints.push(mydata);
+        formatComplaint(mydata,key);
+
+        }
 
     });
 
@@ -96,46 +108,32 @@ function getComplaints(userg){
     });
 
     userComplaints.on('child_removed', function(data) {
+        $('#'+data.key).fadeOut(400, function(){
+            $(this).remove();
+        });
     });
     listeningFirebaseRefs.push(userComplaints);
 
 }
 
-function goToHome(){
-    //redirectLocation = "file:///C:/Users/luke9/Documents/GitHub/Complaint-Manager-Site/public/home.html";
-    redirectLocation = "/home.html";
-    if (window.location.href !== redirectLocation) {
-            window.location.replace(redirectLocation);
 
-    }
-}
-function goToIndex(){
-    //redirectLocation = "file:///C:/Users/luke9/Documents/GitHub/Complaint-Manager-Site/public/index.html";
-    redirectLocation = "/index.html";
-    if (window.location.href !== redirectLocation) {
-            window.location.replace(redirectLocation);
-    }
-}
 function cleanupUi(){
     listeningFirebaseRefs.forEach(function(ref){
         ref.off();
     });
     listeningFirebaseRefs = [];
+    $('#complaint-list').empty();
+    console.log('ui cleanedup');
 }
 
-var currentUID;
+//var currentUID;
 function onAuthStateChanged(user) {
-      if(user === currentUID){
-          console.log('user id matches');
-          return;
-      }
-    currentUID = firebase.auth().currentUser.uid;
+
     if (user) {
-    // User is signed in.
-        goToHome();
-        userProfile();      
+        userProfile();
+        goHome();
+
   } else {
-    // No user is signed in.
 
   }
 }
@@ -144,21 +142,53 @@ function onAuthStateChanged(user) {
 function complaintDate(date){
     var d = new Date(date);
     var formattedD = d.toDateString();
+    var timeDate = new Date(date*1000);
+    var hours = timeDate.getHours();
+    var minutes = timeDate.getMinutes();
+    var formattedTime = hours+':'+minutes;
+
     return formattedD;
 }
-function formatComplaint(complaint){
+function formatComplaint(complaint,key){
+    var resolutionNullText;
+    if (complaint.resolution === null || complaint.resolution === ""){
+        resolutionNullText = "Not yet resolved.";
+    }else{
+        resolutionNullText = complaint.resolution;
+    }
 
-    var html =
-        '<div id="complaint_div">'+
-            '<p id="dateTime">'+complaintDate(complaint.dateTime)+'</p>'+
-            '<p id="name">'+complaint.name+'</p>'+
-            '<p id="complaint">'+complaint.complaint+'</p>'+
+            var html =
+            '<div id="'+key+'">'+
+                '<p id="dateTime">'+complaintDate(complaint.dateTime)+'</p>'+
+                '<p id="name">'+complaint.name+'</p>'+
+                '<p id="location">'+complaint.location+'</p>'+
+                '<p id="complaint">'+complaint.complaint+'</p>'+
+                '<p id="resolution">Resolution: '+resolutionNullText+'</p>'+ 
+                '<p id="authorizedSig">Complaint taken by: '+complaint.authorizedSig+'</p>'+ 
+                '<button id="'+key+'" onClick="replyClick_delete_complaint(this.id)">Remove</button>'+              
+            '</div>';
 
-        '</div>';
+
+            $('#complaint-list').append(html);
+            //$('#'+key).fadeIn('slow');
 
 
-        $('#complaint-list').append(html);
 }
+
+function replyClick_delete_complaint(clicked_id){
+    var ref = firebase.database().ref('Groups/' + userGroup);
+    ref.child(clicked_id).remove();
+
+}
+
+function goHome(){
+    redirectLink = 'home.html';
+    if (window.location.pathname.indexOf('home.html') === -1){
+        window.location.href = redirectLink;
+    }
+}
+
+
 
 $(document).ready(main);
 
